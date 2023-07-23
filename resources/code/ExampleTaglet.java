450bd602dd77e029901bc0c4cbca01b9bedb954d
Sebastian Schemer <sebastianschemer@pm.me>
2023-07-23 09:32:59 +0200

    Formatted a few files

--- resources/code/ExampleTaglet.java
+++ resources/code/ExampleTaglet.java
@@ -40,16 +40,23 @@ import com.sun.tools.doclets.Taglet;
 import com.sun.javadoc.*;
 import java.util.Map;
 import java.io.*;
+
 /**
  * A sample Taglet representing @example. This tag can be used in any kind of
- * {@link com.sun.javadoc.Doc}.  It is not an inline tag. The text is displayed
- * in yellow to remind the developer to perform a task.  For
+ * {@link com.sun.javadoc.Doc}. It is not an inline tag. The text is displayed
+ * in yellow to remind the developer to perform a task. For
  * example, "@example Hello" would be shown as:
  * <DL>
  * <DT>
  * <B>To Do:</B>
- * <DD><table cellpadding=2 cellspacing=0><tr><td bgcolor="yellow">Fix this!
- * </td></tr></table></DD>
+ * <DD>
+ * <table cellpadding=2 cellspacing=0>
+ * <tr>
+ * <td bgcolor="yellow">Fix this!
+ * </td>
+ * </tr>
+ * </table>
+ * </DD>
  * </DL>
  *
  * @author Jamie Ho
@@ -57,7 +64,7 @@ import java.io.*;
  */
 
 public class ExampleTaglet implements Taglet {
-    
+
     private static final String NAME = "example";
     private static final String HEADER = "example To Do:";
 
@@ -67,13 +74,14 @@ public class ExampleTaglet implements Taglet {
     public String getName() {
         return NAME;
     }
-    
+
     /**
      * Will return true since <code>@example</code>
      * can be used in field documentation.
+     * 
      * @return true since <code>@example</code>
-     * can be used in field documentation and false
-     * otherwise.
+     *         can be used in field documentation and false
+     *         otherwise.
      */
     public boolean inField() {
         return true;
@@ -82,31 +90,34 @@ public class ExampleTaglet implements Taglet {
     /**
      * Will return true since <code>@example</code>
      * can be used in constructor documentation.
+     * 
      * @return true since <code>@example</code>
-     * can be used in constructor documentation and false
-     * otherwise.
+     *         can be used in constructor documentation and false
+     *         otherwise.
      */
     public boolean inConstructor() {
         return true;
     }
-    
+
     /**
      * Will return true since <code>@example</code>
      * can be used in method documentation.
+     * 
      * @return true since <code>@example</code>
-     * can be used in method documentation and false
-     * otherwise.
+     *         can be used in method documentation and false
+     *         otherwise.
      */
     public boolean inMethod() {
         return true;
     }
-    
+
     /**
      * Will return true since <code>@example</code>
      * can be used in method documentation.
+     * 
      * @return true since <code>@example</code>
-     * can be used in overview documentation and false
-     * otherwise.
+     *         can be used in overview documentation and false
+     *         otherwise.
      */
     public boolean inOverview() {
         return true;
@@ -115,9 +126,10 @@ public class ExampleTaglet implements Taglet {
     /**
      * Will return true since <code>@example</code>
      * can be used in package documentation.
+     * 
      * @return true since <code>@example</code>
-     * can be used in package documentation and false
-     * otherwise.
+     *         can be used in package documentation and false
+     *         otherwise.
      */
     public boolean inPackage() {
         return true;
@@ -126,106 +138,109 @@ public class ExampleTaglet implements Taglet {
     /**
      * Will return true since <code>@example</code>
      * can be used in type documentation (classes or interfaces).
+     * 
      * @return true since <code>@example</code>
-     * can be used in type documentation and false
-     * otherwise.
+     *         can be used in type documentation and false
+     *         otherwise.
      */
     public boolean inType() {
         return true;
     }
-    
+
     /**
      * Will return false since <code>@example</code>
      * is not an inline tag.
+     * 
      * @return false since <code>@example</code>
-     * is not an inline tag.
+     *         is not an inline tag.
      */
-    
+
     public boolean isInlineTag() {
         return false;
     }
-    
+
     /**
      * Register this Taglet.
-     * @param tagletMap  the map to register this tag to.
+     * 
+     * @param tagletMap the map to register this tag to.
      */
     public static void register(Map tagletMap) {
-       ExampleTaglet tag = new ExampleTaglet();
-       Taglet t = (Taglet) tagletMap.get(tag.getName());
-       if (t != null) {
-           tagletMap.remove(tag.getName());
-       }
-       tagletMap.put(tag.getName(), tag);
+        ExampleTaglet tag = new ExampleTaglet();
+        Taglet t = (Taglet) tagletMap.get(tag.getName());
+        if (t != null) {
+            tagletMap.remove(tag.getName());
+        }
+        tagletMap.put(tag.getName(), tag);
     }
 
     /**
      * Given the <code>Tag</code> representation of this custom
      * tag, return its string representation.
-     * @param tag   the <code>Tag</code> representation of this custom tag.
+     * 
+     * @param tag the <code>Tag</code> representation of this custom tag.
      */
     public String toString(Tag tag) {
         return createHTML(readFile(tag.text()));
     }
-     
-    
+
     /**
      * Given an array of <code>Tag</code>s representing this custom
      * tag, return its string representation.
-     * @param tags  the array of <code>Tag</code>s representing of this custom tag.
+     * 
+     * @param tags the array of <code>Tag</code>s representing of this custom tag.
      */
     public String toString(Tag[] tags) {
         if (tags.length == 0) {
             return null;
         }
-		return createHTML(readFile(tags[0].text()));
+        return createHTML(readFile(tags[0].text()));
     }
-    
-    
-    
-    String createHTML(String theString) {
-    	if(theString!=null) {
-        String dd = "<script type=\"text/javascript\">\n" +
-        			"<!--\n"+ 
-        			"document.getElementsByTagName('html')[0].className = 'isjs';" +
-					"function toggle(dt) { var display, dd=dt; do{ dd = dd.nextSibling } while(dd.tagName!='DD'); toOpen =!dd.style.display;" +
-  					"dd.style.display = toOpen? 'block':''; dt.getElementsByTagName('span')[0].innerHTML  = toOpen? '-':'+' ; }\n" +
-  					"-->\n</script>";
 
-		return dd+"\n<div id=\"test\" class=\"toggleList\">" +
-		"<dl><dt onclick=\"toggle(this);\"><span>+</span>Example</dt>" +
-		"<dd><pre>"+theString+"</pre>" +
-		"</dd></dl></div>";
-		} 
-    	return "";
+    String createHTML(String theString) {
+        if (theString != null) {
+            String dd = "<script type=\"text/javascript\">\n" +
+                    "<!--\n" +
+                    "document.getElementsByTagName('html')[0].className = 'isjs';" +
+                    "function toggle(dt) { var display, dd=dt; do{ dd = dd.nextSibling } while(dd.tagName!='DD'); toOpen =!dd.style.display;"
+                    +
+                    "dd.style.display = toOpen? 'block':''; dt.getElementsByTagName('span')[0].innerHTML  = toOpen? '-':'+' ; }\n"
+                    +
+                    "-->\n</script>";
+
+            return dd + "\n<div id=\"test\" class=\"toggleList\">" +
+                    "<dl><dt onclick=\"toggle(this);\"><span>+</span>Example</dt>" +
+                    "<dd><pre>" + theString + "</pre>" +
+                    "</dd></dl></div>";
+        }
+        return "";
     }
-    
-    
+
     /**
-     * check if the examples directory exists and return the example as given in the tag.
+     * check if the examples directory exists and return the example as given in the
+     * tag.
+     * 
      * @param theExample the name of the example
      */
-     String readFile(String theExample) { 
-		String record = "";
-		String myResult = "";
-		int recCount = 0;
-		String myDir = "../examples";
-		File file=new File(myDir);
-		if(file.exists()==false) {
-			myDir = "./examples";
-		}
-        try { 
-			FileReader fr = new FileReader(myDir+"/"+theExample+"/"+theExample+".pde");
-			BufferedReader br = new BufferedReader(fr);
-			record = new String();
-			while ((record = br.readLine()) != null) {
-				myResult += record+"\n";
-			} 
-		} catch (IOException e) { 
-			System.out.println(e);
-			return null;
-		}
-		return myResult;
-     }
+    String readFile(String theExample) {
+        String record = "";
+        String myResult = "";
+        int recCount = 0;
+        String myDir = "../examples";
+        File file = new File(myDir);
+        if (file.exists() == false) {
+            myDir = "./examples";
+        }
+        try {
+            FileReader fr = new FileReader(myDir + "/" + theExample + "/" + theExample + ".pde");
+            BufferedReader br = new BufferedReader(fr);
+            record = new String();
+            while ((record = br.readLine()) != null) {
+                myResult += record + "\n";
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            return null;
+        }
+        return myResult;
+    }
 }
-
-
--- src/template/library/HelloLibrary.java
+++ src/template/library/HelloLibrary.java
@@ -1,33 +1,31 @@
 package template.library;
 
-
-import processing.core.*;
-
 /**
  * This is a template class and can be used to start a new processing Library.
- * Make sure you rename this class as well as the name of the example package 'template' 
+ * Make sure you rename this class as well as the name of the example package
+ * 'template'
  * to your own Library naming convention.
- * 
- * (the tag example followed by the name of an example included in folder 'examples' will
+ *
+ * (the tag example followed by the name of an example included in folder
+ * 'examples' will
  * automatically include the example in the javadoc.)
  *
- * @example Hello 
+ * @example Hello
  */
 
 public class HelloLibrary {
-	
+
 	// myParent is a reference to the parent sketch
 	PApplet myParent;
 
 	int myVariable = 0;
-	
+
 	public final static String VERSION = "##library.prettyVersion##";
-	
 
 	/**
 	 * a Constructor, usually called in the setup() method in your sketch to
 	 * initialize and start the Library.
-	 * 
+	 *
 	 * @example Hello
 	 * @param theParent the parent PApplet
 	 */
@@ -35,19 +33,18 @@ public class HelloLibrary {
 		myParent = theParent;
 		welcome();
 	}
-	
-	
+
 	private void welcome() {
 		System.out.println("##library.name## ##library.prettyVersion## by ##author##");
 	}
-	
-	
+
 	public String sayHello() {
 		return "hello library.";
 	}
+
 	/**
 	 * return the version of the Library.
-	 * 
+	 *
 	 * @return String
 	 */
 	public static String version() {
@@ -55,7 +52,7 @@ public class HelloLibrary {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param theA the width of test
 	 * @param theB the height of test
 	 */
@@ -64,11 +61,10 @@ public class HelloLibrary {
 	}
 
 	/**
-	 * 
+	 *
 	 * @return int
 	 */
 	public int getVariable() {
 		return myVariable;
 	}
 }
-
